af850470a7da77004bfd854f4cef3977d113658d
Administrador <Administrador@pc01>
2013-04-21 18:44:23 -0500

    commit 3

--- EclipseEGit/src/net/creativoos/git/PruebaEGit.java
+++ EclipseEGit/src/net/creativoos/git/PruebaEGit.java
@@ -3,6 +3,6 @@ package net.creativoos.git;
 public class PruebaEGit {
 	public PruebaEGit(){
 		System.out.println("modif 1");
-		System.out.println("modif 2");
+		System.out.println("modif 3");
 	}
 }
